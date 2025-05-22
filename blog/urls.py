66718a8814c88a61a8a104596ec95f8a28003b76
om-khorwal <amankhorwal250@gmail.com>
2025-05-22 14:47:25 +0530

    blog page according to id

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import BlogPostList, BlogDetailView
 
 urlpatterns = [
-    path('api/', BlogPostList.as_view(), name='blog-list'),
-    path('api/<int:id>/', BlogDetailView.as_view(), name='blog-detail'),
+    path('api/', BlogPostList.as_view(), name='blog-api'),
+    path('api/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
 ]
+    
--- cms_backend/urls.py
+++ cms_backend/urls.py
@@ -19,10 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),  # Important
+    path('blog/', include('blog.urls')),
 ]
 
 if settings.DEBUG:
